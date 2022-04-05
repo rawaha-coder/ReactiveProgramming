d8975e46150cf9447a97e81db807aeea9c831d14
rawaha-coder <rawaha.coder@gmail.com>
2022-04-05 22:48:39 +0000

    finish Subject Project

--- Subjects/src/main/kotlin/Utils.kt
+++ Subjects/src/main/kotlin/Utils.kt
@@ -4,3 +4,11 @@ fun exampleOf(description: String, action: ()-> Unit){
     println("\n--- Example of: $description ---\n")
     action()
 }
+
+fun <T> printWithLabel(label: String, element: T?) {
+    println("$label $element")
+}
+
+sealed class Quote: Throwable() {
+    class NeverSaidThat : Quote()
+}
